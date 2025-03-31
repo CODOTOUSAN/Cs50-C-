b757dc2c72a62c826f7408b352c0b44829c6d380
CODOTOUSAN <97848901+CODOTOUSAN@users.noreply.github.com>
2025-03-30 18:06:22 -0700

    using return 0;

--- Hello-its-me.c
+++ Hello-its-me.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-//#include <cs50.h>
+/*#include <cs50.h>
 
  int main(void)
  {
@@ -8,4 +8,9 @@
   string answer = string("What is your name?\n");
   //Returning a value of name
   printf("Hello, %s\n" answer);
- }
+ }*/
+int main()
+{
+  printf("Hello world"\n);
+  return 0;
+}
